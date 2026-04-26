10cabcdd9a52c25854b8479a2f41e43e239fe693
Javier <710.jag@gmail.com>
2026-04-26 18:57:52 -0300

    Fix Navbar and Footer case sensitivity in Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./navbar/Navbar";
-import Footer from "./footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 const Layout = () => {
   return (
