0d6c1edf97746236715c480dbd8dca1c63181742
Javier <710.jag@gmail.com>
2026-04-28 00:28:20 -0300

    fix: explicit tls version for tidb connection

--- backend/config/config.js
+++ backend/config/config.js
@@ -17,8 +17,8 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'mysql',
+    use_env_variable: "DATABASE_URL",
+    dialect: "mysql",
     dialectOptions: {
       ssl: {
         minVersion: 'TLSv1.2',
